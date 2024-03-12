4c47206fbc9e89406093088419cc9691b9f200fd
Finisherop <160778489+Finisherop@users.noreply.github.com>
2024-03-12 08:03:42 +0530

    Update bot.py

--- MOONMUSIC/core/bot.py
+++ MOONMUSIC/core/bot.py
@@ -10,7 +10,7 @@ class MOON(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="MOONMUSIC",
+            name="NOTTY BOY",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
